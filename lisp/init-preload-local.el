7409337ffc73f509fef737b7c68a6f5a8ce644c1
Tian Liang <tian.liang@chinaott.net>
2017-10-27 15:17:13 +0800

    Add preload support

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,9 @@
+;; (load-theme 'deeper-blue t)			
+(setq ns-pop-up-frames nil)
+
+;; Note directory
+(setq org-default-notes-file "~/Documents/emacs-org/notes.org")
+
+(global-set-key (kbd "C-c =") 'er/expand-region)
+
+(provide 'init-preload-local)
